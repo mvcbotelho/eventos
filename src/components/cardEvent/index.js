b4b9853d6759d084e118b215e6d4d3f65e9b4c1b
Marcus <marcus.botelho@ifactory.com.br>
2019-11-19 00:01:25 -0300

    add functions in cards

--- src/components/cardEvent/index.js
+++ src/components/cardEvent/index.js
@@ -1,19 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import firebase from "../../config/firebase";
 
 import "./styles.css";
 
-function EventCard() {
+function EventCard({ key, image, title, description, views }) {
+  const [urlImage, setUrlImage] = useState();
+
+  useEffect(() => {
+    firebase
+      .storage()
+      .ref(`images/${image}`)
+      .getDownloadURL()
+      .then(url => setUrlImage(url));
+  });
   return (
     <div className="col-md-3 col-sm-12">
       <img
-        src="https://via.placeholder.com/600"
+        src={urlImage}
         alt="banner de divulgação"
         className="card-img-top card-img"
       />
       <div className="card-body">
-        <h5>Título do evento</h5>
-        <p className="card-text text-justify">detalhes do evento</p>
+        <h5>{title}</h5>
+        <p className="card-text text-justify">{description}</p>
         <div className="row base-card d-flex align-items-center">
           <div className="col-6">
             <Link to="" className="btn btn-sm btn-detail">
@@ -22,7 +32,7 @@ function EventCard() {
           </div>
           <div className="col-6 text-right">
             <i className="far fa-eye"></i>
-            <span> 200</span>
+            <span> {views}</span>
           </div>
         </div>
       </div>
--- src/view/home/index.js
+++ src/view/home/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-// import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import firebase from "../../config/firebase";
 
 // import "./styles.css";
 
@@ -8,11 +8,40 @@ import NavBar from "../../components/navbar/";
 import CardEvent from "../../components/cardEvent/";
 
 function Home() {
+  const [eventos, setEventos] = useState([]);
+  let listaEventos = [];
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("eventos")
+      .get()
+      .then(async res => {
+        await res.docs.forEach(doc => {
+          listaEventos.push({
+            id: doc.id,
+            ...doc.data()
+          });
+        });
+        setEventos(listaEventos);
+      })
+      .catch();
+  });
   return (
     <>
       <NavBar />
       {useSelector(state => state.userStatus) > 0 ? <h1>logado</h1> : null}
-      <CardEvent />
+      <div className="row p-3">
+        {eventos.map(item => (
+          <CardEvent
+            key={item.id}
+            image={item.image}
+            title={item.title}
+            description={item.description}
+            views={item.views}
+          />
+        ))}
+      </div>
     </>
   );
 }
